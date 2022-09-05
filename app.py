0b8d52bbe97481c05dc07d79ab84484c0e455006
Fatimah Mohammed <fmg1413@hotmail.com>
2022-09-05 10:03:10 +0300

    push

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
