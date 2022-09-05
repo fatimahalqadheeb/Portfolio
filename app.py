1d84d0ee35073f2e855dd5afbaf34c185b1b5a78
Fatimah Mohammed <fmg1413@hotmail.com>
2022-09-05 21:13:21 +0300

    setup home

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
