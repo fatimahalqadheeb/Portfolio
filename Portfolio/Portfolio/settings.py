fd2403264e6690cf6746c1515666f111f0323491
Fatimah Mohammed <fmg1413@hotmail.com>
2022-10-23 09:10:49 +0300

    add python version

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 
 # Default primary key field type
