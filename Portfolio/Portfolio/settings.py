14dd3b5f205a8470d1c3cc479505d43b39dfd588
Fatimah Mohammed <fmg1413@hotmail.com>
2022-10-23 10:18:01 +0300

    code

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+django_on_heroku.settings(locals(), staticfiles=false)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
