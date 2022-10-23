62b62ebb563e678ec222208e2d412de177a927b2
Fatimah Mohammed <fmg1413@hotmail.com>
2022-10-23 08:58:00 +0300

    setup heroku

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ck5)wt^fx7+fv!7um9eu&q_%-&easmyra(-3o=%bby49e(5ni2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
