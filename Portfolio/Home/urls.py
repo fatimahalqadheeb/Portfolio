a130ef73a8330753fd3cdeb767d6908b47cf640f
Fatimah Mohammed <fmg1413@hotmail.com>
2022-10-23 12:17:46 +0300

    remove backslash

--- Portfolio/Home/urls.py
+++ Portfolio/Home/urls.py
@@ -5,7 +5,7 @@ app_name = 'Home'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/about', views.about, name='about'),
-    path('/projects', views.projects, name='projects'),
-    path('/contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+    path('projects', views.projects, name='projects'),
+    path('contact', views.contact, name='contact'),
 ]
