f24245c211ace24dad7cede09779648e75b88429
Fatimah Mohammed <fmg1413@hotmail.com>
2022-11-23 11:05:12 +0300

    rearrange the folders

---
+++ Home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+
+# Create your views here.
+
+
+def index(request: HttpRequest):
+    return render(request, 'Home/home.html')
+
+
+def about(request: HttpRequest):
+    return render(request, 'Home/about.html')
+
+
+def projects(request: HttpRequest):
+    return render(request, 'Home/projects.html')
+
+
+def contact(request: HttpRequest):
+    return render(request, 'Home/contact.html')
